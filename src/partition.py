295f2b78bfad9029ce4e9bc89ce2cedcb62c2d9c
Nhat Xuan <nhatxuaan@example.com>
2026-05-17 03:30:19 +0700

    fix: alpha from 100 to 0.5

--- src/partition.py
+++ src/partition.py
@@ -150,7 +150,7 @@ if __name__ == "__main__":
         np.arange(num_samples),
         labels,
         num_clients=num_clients,
-        alpha=100,
+        alpha=0.5,
         seed=42,
     )
     
